cdbf2adc305772804f97e03a886aec12ecf30938
Jordi Torrents <jordi.t21@gmail.com>
2024-02-29 11:56:09 +0100

    [Ruby] Tweak ruby indentation settings.

--- init.lua
+++ init.lua
@@ -477,8 +477,9 @@ require('nvim-treesitter.configs').setup {
 		-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
 		-- Using this option may slow down your editor, and you may see some duplicate highlights.
 		-- Instead of true it can also be a list of languages
-		additional_vim_regex_highlighting = { 'markdown' },
+		additional_vim_regex_highlighting = { 'markdown', 'ruby' },
   },
+  -- For ruby, every time a dot '.' is typed, Treesitter unconditionally dedents (de-indents).
   indent = { enable = true, disable = { 'ruby' } },
   endwise = { enable = true },
   incremental_selection = {
